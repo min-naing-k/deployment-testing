80576e0f6a898a4e2a7920ca2d268fa338fa666e
minnaingkyaw <minnaingmdy436@gmail.com>
2022-09-29 17:33:08 +0630

    add some code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
 
 <body>
     <h1 style="text-align: center;font-family: Nunito;font-weight: 400">
-        Deployment Testing Hola Hi
+        Deployment Testing Hola Hi Hi Hi
     </h1>
 </body>
 
